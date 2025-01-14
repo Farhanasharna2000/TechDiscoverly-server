a1b6eec6ac97468e9182f446579420a690df0989
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-01-14 23:27:23 +0600

    post user data in db

--- index.js
+++ index.js
@@ -67,7 +67,18 @@ async function run() {
       next()
     }
  
-     
+    //post users collection in db
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exist', insertedId: null })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
