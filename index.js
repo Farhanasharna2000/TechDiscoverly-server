dc1c0ccaeea0735e59356bc68264cacfb67640fe
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-02-19 10:06:15 +0600

    update

--- index.js
+++ index.js
@@ -334,7 +334,7 @@ async function run() {
 
     //get trendingProducts data from db
     app.get('/trendingProducts', async (req, res) => {
-      const result = await productsCollection.find({ isAccepted: true }).sort({ upvoteCount: -1 }).limit(6).toArray();
+      const result = await productsCollection.find({ isAccepted: true }).sort({ upvoteCount: -1 }).limit(8).toArray();
       res.send(result);
     })
 
